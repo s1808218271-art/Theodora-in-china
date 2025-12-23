15056fbd0f6d9bdda0e8cd322aec3b3b2ecb8a1e
s1808218271-art <s.1808218271@gmail.com>
2025-12-23 09:43:24 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // 确保构建时能够正确处理根目录
-  base: './', 
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html')
+      }
+    }
+  }
 })
