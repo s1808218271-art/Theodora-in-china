7f69523187805176581f711ba78cc7423697c7aa
s1808218271-art <s.1808218271@gmail.com>
2025-12-23 14:50:07 +0800

    Delete main.jsx

--- main.jsx
+++
@@ -1,6 +0,0 @@
-// main.jsx
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'  // 注意大小写要和文件名完全一致
-
-createRoot(document.getElementById('root')).render(<App />)
